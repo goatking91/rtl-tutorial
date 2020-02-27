9372f34f1e0dc16c4b78a82f6b545317d08207ea
Selim.Yeom <selim.yeom@lfin.kr>
2020-02-27 12:09:46 +0900

    Project Init

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'react/prefer-stateless-function': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/no-array-index-key': 0,
+    'arrow-body-style': 0,
+    'no-console': 0,
+    'no-alert': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.tsx', '**/test.ts'] }],
+    '@typescript-eslint/indent': [2, 2],
+  },
+};
