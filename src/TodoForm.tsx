078082eeeeac51abb8b609d4d35deea706196d70
Selim.Yeom <selim.yeom@lfin.kr>
2020-02-27 13:59:55 +0900

    TodoForm 개발하기

---
+++ src/TodoForm.tsx
@@ -0,0 +1,31 @@
+import React, { useCallback, useState } from 'react';
+
+type TodoFormProps = {
+  onInsert: Function;
+};
+
+const TodoForm: React.FC<TodoFormProps> = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback(e => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    e => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input placeholder="할 일을 입력하세요" value={value} onChange={onChange} />
+      <button type="submit">등록</button>
+    </form>
+  );
+};
+
+export default TodoForm;
